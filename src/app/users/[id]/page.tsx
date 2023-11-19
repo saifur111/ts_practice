01884d1f385252c91680504d7f7d94188121564c
saifur-rahman98 <mdsaifur.cse.98@gmail.com>
2023-11-20 00:33:41 +0600

    Here Featching client side data npm package swr this one reduce our code

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,27 +1,40 @@
 'use client'
 import { useParams, useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+import useSWR from 'swr';
+
+
 
 const UserInfopage = () => {
-   const router =useRouter();
-   const [singleUser,setSingleUser]=useState([]);
+//    const router =useRouter();
+//    const [singleUser,setSingleUser]=useState([]);
    const {id}= useParams();
+   const userFetcher =(...arg)=>fetch(...arg).then((res)=>res.json());
 
-   useEffect(()=>{
+//    useEffect(()=>{
         
-        async function getUserById(id) {
-            const data =await fetch(`https://dummyjson.com/users/${id}`);
+//         async function getUserById(id) {
+//             const data =await fetch(`https://dummyjson.com/users/${id}`);
             
-            setSingleUser(await data.json())
-        }
-        getUserById(id);
-   },[])
+//             setSingleUser(await data.json())
+//         }
+//         getUserById(id);
+//    },[])
+   const {data,error}=useSWR(`https://dummyjson.com/users/${id}`, userFetcher)
+    if(error){
+    return <h1>Error To Fetching Data...</h1>
+}
+    if(!data) return <h1>Loading Data...</h1>
 
   return (
     <div>
-        <h1>UserInfopage</h1>
+        {/* <h1>UserInfopage</h1>
         <h2>{singleUser?.firstName}</h2>
-        <h2>{singleUser?.email}</h2>
+        <h2>{singleUser?.email}</h2> */}
+
+        <h1>UserInfopage</h1>
+        <h2>{data?.firstName}</h2>
+        <h2>{data?.email}</h2>
 
         
     </div>
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,26 +1,45 @@
 "use client"
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+import useSWR from 'swr';
+
+const userFetcher =(...arg)=>fetch(...arg).then((res)=>res.json());
 
 const Users = () => {
-    const [users,setUsers]=useState([]);
-    console.log(users);
-    useEffect(()=>{
-        async function fetchUsers() {
-            const data =await fetch("https://dummyjson.com/users");
+    // const [users,setUsers]=useState([]);
+    // console.log(users);
+    // useEffect(()=>{
+    //     async function fetchUsers() {
+    //         const data =await fetch("https://dummyjson.com/users");
             
-            setUsers(await data.json())
+    //         setUsers(await data.json())
             
-        }
-        fetchUsers();
-
-    },[])
+    //     }
+    //     fetchUsers();
+    const {data,error}=useSWR('https://dummyjson.com/users', userFetcher)
+    if(error){
+        return <h1>Error To Fetching Data...</h1>
+    }
+    if(!data) return <h1>Loading Data...</h1>
+    // },[])
   return (
     <div>
-        <h1>Users</h1>
+        {/* <h1>Users</h1>
         {users&&
         users?.users&&
         users?.users.map((user)=>(
+            <div key={user.id}>
+                <Link  href={`/users/${user.id}`}> 
+                <div>{user.firstName}</div>
+                </Link>
+            </div>
+        ))
+        
+        } */}
+        <h1>Users</h1>
+        {data&&
+        
+        data?.users.map((user)=>(
             <div key={user.id}>
                 <Link  href={`/users/${user.id}`}> 
                 <div>{user.firstName}</div>
